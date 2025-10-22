854061b34a66dd0af32de6798ca4a5b3ae586f7c
Ahmad Zuhal Zhafran <90975889+azuhalz@users.noreply.github.com>
2025-10-22 16:31:56 +0700

    first commit

---
+++ FinalChallenge/FinalChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinalChallengeApp.swift
+//  FinalChallenge
+//
+//  Created by Ahmad Zuhal Zhafran on 22/10/25.
+//
+
+import SwiftUI
+
+@main
+struct FinalChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
