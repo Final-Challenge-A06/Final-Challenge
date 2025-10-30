45e14cc7714515b33d03120e7fd15a0f4eac9221
AngelApriliaPutri <angelaprilia2004@gmail.com>
2025-10-30 19:53:10 +0700

    set goals input swift data

---
+++ UnitTest/UnitTest.swift
@@ -0,0 +1,16 @@
+//
+//  UnitTest.swift
+//  UnitTest
+//
+//  Created by Angel Aprilia Putri Lo on 30/10/25.
+//
+
+import Testing
+
+struct UnitTest {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
