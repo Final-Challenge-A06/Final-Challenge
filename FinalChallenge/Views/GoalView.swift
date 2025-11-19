f4cf46960acec93aade12b2c7c367649ccb78646
AngelApriliaPutri <angelaprilia2004@gmail.com>
2025-11-19 15:46:00 +0700

    fix: goalmodal

--- FinalChallenge/Views/GoalView.swift
+++ FinalChallenge/Views/GoalView.swift
@@ -196,29 +196,6 @@ struct GoalView: View {
                 .zIndex(5)
             }
             circleClaimOverlay
-            // Circle step claim modal
-//            if showCircleClaimModal, let step = pendingCircleClaimStep {
-//                CenteredModal(isPresented: $showCircleClaimModal) {
-//                    if let meta = getRewardMeta(for: step.id) {
-//                        ClaimModalView(
-//                            title: meta.title,
-//                            imageName: meta.imageName,
-//                            onClaim: {
-//                                goalVm.openClaim(for: meta, context: context)
-//                                goalVm.confirmClaim(context: context)
-//                                goalVm.loadRewardsForView(context: context)
-//                                bottomItemsVM.setItems(goalVm.rewardViewItems)
-//                                let currentGoalStepsList = goals.map { $0.totalSteps }
-//                                let claimedSteps = goalVm.getClaimedSteps(context: context)
-//                                circleVM.updateSteps(goalSteps: currentGoalStepsList, passedSteps: goalVm.passedSteps, claimedSteps: claimedSteps)
-//                                showCircleClaimModal = false
-//                                pendingCircleClaimStep = nil
-//                            }
-//                        )
-//                    }
-//                }
-//                .zIndex(6)
-//            }
         }
         .onAppear {
             // Buat StreakManager sekali
